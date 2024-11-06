7435956a1c2c711d9ad4683cdffe92d119beaaa1
0xdeadcode <dlaczegojesttakcieplo@gmail.com>
2024-11-06 11:39:20 +0000

    feat: initial add dsl

---
+++ dsl/scripts/directories.groovy
@@ -0,0 +1,38 @@
+def dirs = [
+    [
+        id: 'private',
+        display: '[Private]',
+    ],
+    [
+        id: 'private/infra',
+        display: '[Infra automations]',
+    ],
+    // [
+    //     id: 'common',
+    //     display: 'Common',
+    //     permissions: [
+    //         'anonymous': [
+    //             'hudson.model.View.Read',
+    //             'hudson.model.Item.Read',
+    //         ],
+    //     ],
+    // ],
+]
+
+dirs.each { directory ->
+    folder(directory.id){
+        if (directory.display) {
+            displayName(directory.display)
+        }
+        if (directory.description) {
+            description(directory.description)
+        }
+        if (directory.permissions) {
+            authorization {
+                directory.permissions.each { user, userPermissions ->
+                    permissions(user, userPermissions)
+                }
+            }
+        }
+    }
+}
