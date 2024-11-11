6273b86d1fa80919027dc4d5f2f17ab58117e752
0xdeadcode <dlaczegojesttakcieplo@gmail.com>
2024-11-11 17:51:09 +0000

    feat: send slack message from snapshot-testing pipeline

--- vars/pipelineSnapshotTesting.groovy
+++ vars/pipelineSnapshotTesting.groovy
@@ -8,6 +8,8 @@ void call(Map config=[:]) {
     String nodeLabel = "snapshot-testing"
     String networkName = env.NET_NAME
     String configPath = ""
+    Boolean shouldSkipNotificationMessage = false
+    String extraLogLines = ""
 
     if (config.containsKey('networkName')) {
         networkName = config.networkName
@@ -121,7 +123,6 @@ void call(Map config=[:]) {
                 currentBuild.result = 'SUCCESS'
                 reason = "Unknown failure"
                 String catchupDuration = "N/A"
-                String extraLogLines = ""
 
                 Map results = [:]
                 if (fileExists('./work-dir/results.json')) {
@@ -162,13 +163,7 @@ void call(Map config=[:]) {
                 }
                 
                 // We have conditions (e.g: when the devnet1 network is dead) to not report it
-                Boolean shouldSkipSlackMessage = (results["should-skip-failure"] as Boolean) ?: false
-
-                if (!shouldSkipSlackMessage) {
-                    sendSlackMessage(networkName, reason, catchupDuration, extraLogLines)
-                } else {
-                    print("Not sending slack message. Snapshot testing binary decided to skip the results.")
-                }
+                shouldSkipNotificationMessage = (results["should-skip-failure"] as Boolean) ?: false
             }
         }
 
@@ -189,43 +184,36 @@ void call(Map config=[:]) {
                 ].join(','),
             )
         }
+
+        stage('Send notifications') {
+            environment {
+                DISCORD_WEBHOOK_URL = credentials('nebula-discord-webhook-url')
+            }
+
+            when {
+                currentBuild.currentResult.toLowerCase() != 'success' && !shouldSkipNotificationMessage
+            }
+
+            script {
+                String result = currentBuild.currentResult.toLowerCase()
+
+                String description = "No extra logs has been reported. See the pipeline logs for details"
+                if (extraLogLines.length() > 0) {
+                    description = '''Node potentially failed with the following error:
+```
+''' + extraLogLines + '''
+```
+                    '''
+                }
+
+                discordSend webhookURL: env.DISCORD_WEBHOOK_URL,
+                            title: 'Snapshot testing(' + env.NET_NAME + ') #' + env.BUILD_NUMBER,
+                            result: currentBuild.currentResult,
+                            link: env.BUILD_URL,
+                            description: description + "\n\u2060", // word joiner character forces a blank line
+                            enableArtifactsList: true,
+                            showChangeset: false
+            }
+        }
     }
 }
-
-// void archiveArtifactsToS3(int buildNo, String envName, String dirName, String minSnapshot, String maxSnapshot) {
-//     // We are not interested in archiving non mainnet chain data
-//     if (envName != "nebula1") {
-//         return
-//     }
-
-//     // We do not need to archive all artifacts. People can replay if needed.
-//     if (buildNo % 4 != 0) {
-//         return
-//     }
-
-//     Date date = new Date()
-//     SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd_HHmmss")
-//     String dateString = sdf.format(date)
-
-//     String snapshotName = dateString + '-' + minSnapshot + '-' + maxSnapshot + '.tar.gz'
-
-//     sh '''tar \
-//         -I 'gzip -9' \
-//         --exclude ''' + dirName + '''/vega.sock \
-//         --exclude ''' + dirName + '''/bins \
-//         --exclude ''' + dirName + '''/vega_home/state/data-node/networkhistory/store/ipfs/blocks \
-//         -cvf  ''' + snapshotName + ''' \
-//     ./''' + dirName
-
-//      withCredentials([
-//         usernamePassword(
-//             credentialsId:  envName.toLowerCase() + '-snapshot-history-aws-key', 
-//             passwordVariable: 'AWS_SECRET_ACCESS_KEY', 
-//             usernameVariable: 'AWS_ACCESS_KEY_ID'
-//         ),
-//         string(credentialsId: envName.toLowerCase() + '-snapshot-history-bucket-name', variable: 'BUCKET_NAME')
-//     ]) {
-//         sh 'GOBIN="$(pwd)" go install github.com/opendevsecops/go-s3cp@8b07da75e3cd053a4543423a18605358e696fefe'
-//         sh '''AWS_REGION=us-east-1 ./go-s3cp --from ./''' + snapshotName + ''' --to s3://''' + BUCKET_NAME + '''/''' + snapshotName
-//     }
-// }
